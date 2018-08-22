1d40f059ff94e23675fc4c1602531d8c477b4bfa
mdmiller11 <mmille2@stevens.edu>
2018-08-22 19:59:57 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -4,17 +4,17 @@ import numpy as np
 import signal
 from picamera import PiCamera
 from time import sleep
-from datetime import datetime
+import datetime
 from pusher import Pusher
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 LOG = logging.getLogger("capture_motion")
 
-seconds_between_photos = 15
 pusher_app_id = '568536'
-pusher_key = '3fd41d2359eef671c1c'
+pusher_key = '3fd4f1d2359eef671c1c'
 pusher_secret = 'a34d43be28b5322a7d9d'
-hostname =  'https://dd2f55bc.ngrok.io'
+#Replace hostname each time program is run.
+hostname =  'https://45dce350.ngrok.io'
 
 camera = PiCamera()
 
@@ -22,7 +22,6 @@ pusher = Pusher(pusher_app_id, pusher_key, pusher_secret)
 
 def signal_term_handler(signal, frame):
   LOG.info('shutting down ...')
-  # this raises SystemExit(0) which fires all "try...finally" blocks:
   sys.exit(0)
   
 signal.signal(signal.SIGTERM, signal_term_handler)
@@ -40,39 +39,36 @@ class DetectMotion(picamera.array.PiMotionAnalysis):
         np.square(a['x'].astype(np.float)) +
         np.square(a['y'].astype(np.float))
       ).clip(0, 255).astype(np.uint8)
-      # experiment with the following "if" as it may be too sensitive ???
-      # if there're more than 10 vectors with a magnitude greater
-      # than 60, then motion was detected:
-      if (a > 60).sum() > 10:
+      
+      if (a > 75).sum() > 10:
         LOG.info('motion detected at: %s' % datetime.datetime.now().strftime('%Y-%m-%dT%H.%M.%S.%f'))
         motion_detected = True
 
 
-# If you need to rotate the camera
-# camera.rotation = 180
+
 
 with DetectMotion(camera) as output:
     try:
         camera.resolution = (640, 480)
+        camera.rotation = 180
         camera.framerate= 10
-        # record video to nowhere, as we are just trying to capture images:
         camera.start_recording('/dev/null', format='h264', motion_output=output)
         while True:
             while not motion_detected:
                 LOG.info('waiting for motion...')
                 camera.wait_recording(1)
 
-        LOG.info('stop recording and capture an image...')
-        camera.stop_recording()
-        motion_detected = False
+            LOG.info('stop recording and capture an image...')
+            camera.stop_recording()
+            motion_detected = False
       
-        date = datetime.now().strftime('%m-%d-%Y-%H:%M:%S')
-        camera.annotate_text = date
-        filename = '/photos/' + date + '.jpg'
-        camera.capture('/var/www/html' + filename)
-        url = hostname + filename
-        pusher.trigger('photos', 'new_photo', {'url': url}) 
-        camera.start_recording('/dev/null', format='h264', motion_output=output)
+            date = datetime.datetime.now().strftime('%m-%d-%Y-%H:%M:%S')
+            camera.annotate_text = date
+            filename = '/photos/' + date + '.jpg'
+            camera.capture('/var/www/html' + filename)
+            url = hostname + filename
+            pusher.trigger('photos', 'new_photo', {'url': url}) 
+            camera.start_recording('/dev/null', format='h264', motion_output=output)
 
     except KeyboardInterrupt as e:
         LOG.info("\nreceived KeyboardInterrupt via Ctrl-C")
@@ -81,5 +77,4 @@ with DetectMotion(camera) as output:
         camera.close()
         LOG.info("\ncamera turned off!")
         LOG.info("detect motion has ended.\n")
-
 
